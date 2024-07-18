6a573666ce22cfd7a5b134cb36ea655b4a6ebc69
GovardhanArikatla7456 <govardhanarikatla74@.com>
2024-07-18 13:57:43 -0400

    production mode

--- travelogue_backend/settings.py
+++ travelogue_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jooazex9_w%gqqlw)i=h@%pr^2adthuza)n0-(xpe)%8gebztp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
